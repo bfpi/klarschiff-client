3db0f870e4ab95287025037007e3aa6af4f9f049
Niels Bennke <bennke@bfpi.de>
2015-04-01 09:24:05 +0200

    Konfiguration: Zeitzone, Sprache, Layout für dom_id

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,13 @@ module KlarschiffFieldService
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :de
+    
+    ActionView::RecordIdentifier.send(:remove_const, "JOIN")
+    ActionView::RecordIdentifier.const_set("JOIN", "-")
   end
 end
