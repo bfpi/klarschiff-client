aa27f5b23eb1578d971fabc791c19df241207b55
Niels Bennke <bennke@bfpi.de>
2015-04-07 14:34:00 +0200

    Bessere Ausgabe des detailierten Status eines Vorgangs

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -14,4 +14,12 @@ module RequestsHelper
       content_tag :span, nil, class: 'glyphicon glyphicon-camera', "aria-hidden" => true
     end if request.extended_attributes.photo_required
   end
+
+  def status(request)
+    status = t(request.extended_attributes.detailed_status.downcase, scope: :status)
+    if date = request.extended_attributes.detailed_status_datetime
+      status << " (#{ t('requests.status.since') } #{ l(date.to_date) })"
+    end
+    status << ", #{ t('requests.status.currently') } #{ request.agency_responsible }"
+  end
 end
