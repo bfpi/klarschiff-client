dde35899d3d5d5b0bb13cb8359c8cc6ec56216c2
ro3t <voss@bfpi.de>
2015-04-15 16:24:43 +0200

    Neu laden der Features auf die Aktuelle BBox einschränken

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,6 +3,9 @@ class RequestsController < ApplicationController
     conditions = { agency_responsible: @user.field_service_team}
     conditions[:service_request_id] = params[:ids].join(",") if params[:ids]
     conditions[:negation] = "agency_responsible"
+    conditions[:lat] = params[:center][0] if params[:center]
+    conditions[:long] = params[:center][1] if params[:center]
+    conditions[:radius] = params[:radius] if params[:radius]
     @requests = Request.where(conditions)
     @requests = @requests.try(:to_a)
     respond_to do |format|
