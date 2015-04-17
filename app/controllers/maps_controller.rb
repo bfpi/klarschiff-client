de7a05e5bdb48b2626ea648f454eb3a672d6e47c
ro3t <voss@bfpi.de>
2015-04-17 12:54:09 +0200

    Karte nach Standortsuche auf den Bereich verschieben

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,5 +2,6 @@ class MapsController < ApplicationController
   def show
     @requests = Request.all.try(:to_a)
     @show_non_job_features = !params[:type].eql?("jobs")
+    @bbox = params[:bbox] if params[:bbox].present?
   end
 end
--- app/views/maps/show.js.coffee
+++ app/views/maps/show.js.coffee
@@ -1,3 +1,7 @@
 KS.nav.switchTo 'map'
 
 KS.layers.findById("features").setVisible(<%= @show_non_job_features %>)
+
+<% if @bbox.present? -%>
+KS.olMap.getView().fitExtent Array(<%= @bbox.join ", " %>), KS.olMap.getSize()
+<% end -%>
