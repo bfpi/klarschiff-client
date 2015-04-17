02892f44e52952b57d7b9d3ba33b7c45d700494b
Niels Bennke <bennke@bfpi.de>
2015-04-17 08:36:40 +0200

    Response-Aufteilung passiert automatisch

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,9 +2,5 @@ class MapsController < ApplicationController
   def show
     @requests = Request.all.try(:to_a)
     @show_non_job_features = !params[:type].eql?("jobs")
-    respond_to do |format|
-      format.js
-      format.html
-    end
   end
 end
