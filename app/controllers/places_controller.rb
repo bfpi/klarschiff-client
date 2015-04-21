98479ac59c73169ea97de0f26572a58997e8cc16
Niels Bennke <bennke@bfpi.de>
2015-04-21 14:28:13 +0200

    Die letzte Suche merken und das Ergebnis beim erneutem Besuch wieder anzeigen.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,19 +1,27 @@
 class PlacesController < ApplicationController
   def index
-    require 'open-uri'
-    if (@pattern ||= params[:pattern]).present?
-      uri = URI(KS_FRONTEND_SEARCH_URL)
-      uri.query = URI.encode_www_form(searchtext: @pattern)
+    respond_to do |format|
+      format.html { head(:not_acceptable) }
+      format.js do
+        if (@pattern = params[:pattern]).present?
+          session[:places_filter] = @pattern
+        else
+          @pattern = session[:places_filter]
+        end
+        if @pattern.blank?
+          return head(:ok) if params[:pattern]
+        else
+          require 'open-uri'
+          uri = URI(KS_FRONTEND_SEARCH_URL)
+          uri.query = URI.encode_www_form(searchtext: @pattern)
 
-      if (res = open(uri)) && res.status.include?('OK')
-        @places = Array.wrap(JSON.parse(res.read).try(:[], 'array')).map do |p|
-          Place.new p
+          if (res = open(uri)) && res.status.include?('OK')
+            @places = Array.wrap(JSON.parse(res.read).try(:[], 'array')).map do |p|
+              Place.new p
+            end
+          end
         end
       end
-    end
-    respond_to do |format|
-      format.html { head :forbidden }
-      format.js
     end
   end
 end
