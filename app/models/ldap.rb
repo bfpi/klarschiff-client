447822450c3e7a6e26c371164eedac75e80d9c71
Niels Bennke <bennke@bfpi.de>
2015-04-30 08:22:09 +0200

    Abruf der Gruppenbezeichnung für das Aussendienstteam aus dem LDAP

--- app/models/ldap.rb
+++ app/models/ldap.rb
@@ -1,25 +1,33 @@
 class Ldap
-  def self.config
-    @@config ||= YAML.load(File.new(File.join(
-      Rails.root, "config", "ldap.yml"
-    )))[Rails.env].with_indifferent_access
-  end
-
-  def self.login(username, pwd)
-    ldap = Net::LDAP.new(config.slice(:host, :port, :encryption))
-    ldap.auth "cn=#{ username },#{ config[:base] }", pwd
-    if ldap.bind && (
-        user = ldap.search(config.slice(:base).merge(filter: "cn=#{ username }")).try(:first))
-      return User.new(name: user.displayname.first, email: user.mail.first)
+  class << self
+    def config
+      @@config ||= File.open(Rails.root.join('config', 'settings.yml')) { |file|
+        YAML::load file }['ldap'].with_indifferent_access
     end
-  end
 
-  def self.login2(name)
-    ldap = Net::LDAP.new(config.slice(:host, :port, :encryption))
-    ldap.auth *config.slice(:username, :password).values
-    if ldap.bind && (
-        user = ldap.search(config.slice(:base).merge(filter: "displayname=#{ name }")).try(:first))
-      return User.new(name: user.displayname.first, email: user.mail.first)
+    def login(username, pwd)
+      ldap = Net::LDAP.new(config.slice(:host, :port, :encryption))
+      ldap.auth "cn=#{ username },#{ config[:base][:users] }", pwd
+      if ldap.bind && (
+          obj = ldap.search(base: config[:base][:users], filter: "cn=#{ username }").try(:first))
+        return user(ldap, obj)
+      end
+    end
+
+    def login2(name)
+      ldap = Net::LDAP.new(config.slice(:host, :port, :encryption))
+      ldap.auth *config.slice(:username, :password).values
+      if ldap.bind && (
+          obj = ldap.search(base: config[:base][:users], filter: "displayname=#{ name }").try(:first))
+        return user(ldap, obj)
+      end
+    end
+
+    def user(ldap, obj)
+      User.new name: obj.displayname.first, email: obj.mail.first,
+        field_service_team: ldap.search(base: config[:base][:groups],
+                                        filter: config[:group_search_pattern] % obj.dn
+                                       ).try(:first).try(:cn).try(:first).presence
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User
   include ActiveModel::Model
 
   attr_accessor :name, :email, :field_service_team
-
-  def field_service_team # TODO: aus LDAP lesen
-    'A-Team'
-  end
 end
