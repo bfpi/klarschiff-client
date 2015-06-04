73a4de16a90781d5152c07dfb2352d4eed1c5d03
Niels Bennke <bennke@bfpi.de>
2015-06-04 14:06:08 +0200

    Fix Redmine #520 Bei Abfrage der Meldungen vom Server den konfigurierten Standardfilter für die Status beachten

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,6 +4,9 @@ class JobsController < ApplicationController
     if (center = params[:center]).present?
       conditions.update lat: center[0], long: center[1]
     end
+    if (states = Settings::Map.default_requests_states).present?
+      conditions.update detailed_status: states
+    end
     @jobs = Request.where(conditions.merge(params.slice(:radius))).try(:to_a)
     session[:referer_params] = params.slice(:controller, :action, :ids)
     session[:id_list] = @jobs.map(&:id)
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,6 +5,9 @@ class RequestsController < ApplicationController
     if (center = params[:center]).present?
       conditions.update lat: center[0], long: center[1]
     end
+    if (states = Settings::Map.default_requests_states).present?
+      conditions.update detailed_status: states
+    end
     @requests = Request.where(conditions.merge(params.slice(:radius))).try(:to_a)
     session[:referer_params] = params.slice(:controller, :action, :ids)
     respond_to do |format|
