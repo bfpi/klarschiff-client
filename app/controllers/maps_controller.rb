a7d4530fcb6f534ea9de1538a2d7b851828fc788
BFPI <info@bfpi.de>
2015-09-22 11:02:04 +0200

    Performance: Unnötiges Laden aller Meldungen entfernt

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,5 @@
 class MapsController < ApplicationController
   def show
-    @requests = Request.all.try(:to_a)
     @show_non_job_features = !params[:type].eql?("jobs")
     @bbox = params[:bbox] if params[:bbox].present?
   end
