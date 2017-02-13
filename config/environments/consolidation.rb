e7234bfc3fcacae150f533afa896a8b8f9f3c624
ro90 <oest@bfpi.de>
2017-02-13 13:44:55 +0100

    deprecated Methode entfernt

--- config/environments/consolidation.rb
+++ config/environments/consolidation.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_controller.relative_url_root = '/field_service'
+  #config.action_controller.relative_url_root = '/mobil'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -23,7 +23,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :closure
