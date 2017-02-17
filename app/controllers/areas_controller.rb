d0566104c87d87b55f3066cd8663a75c676a9f0e
Ricardo Oest <oest@bfpi.de>
2017-02-17 07:12:04 +0100

    Anzeigen der Stadtteile im Beobachtungs-Dialog für Stadtteile

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,8 +1,15 @@
 class AreasController < ApplicationController
   def index
-    conds = {}
-    conds.update area_code: Array.wrap(params[:area_code]).join(', ')
-    conds.update with_districts: params[:with_districts] unless params[:with_districts].blank?
-    @areas = Area.where(conds)
+    unless (@show_districts = params[:show_districts] || @cancel = params[:cancel]).present?
+      conds = {}
+      conds.update area_code: Array.wrap(params[:area_code]).join(', ')
+      conds.update with_districts: params[:with_districts] unless params[:with_districts].blank?
+      @areas = Area.where(conds)
+    end
+    respond_to do |format|
+      format.html { head :not_acceptable }
+      format.js
+      format.json { render json: @areas }
+    end
   end
 end
---
+++ app/views/areas/index.js.coffee
@@ -0,0 +1,8 @@
+<% if @show_districts -%>
+KS.layers.findById('observations').setVisible(true)
+$('.area-container').replaceWith("<%= j render partial: 'districts_form' %>")
+<% end -%>
+<% if @cancel -%>
+KS.layers.findById('observations').setVisible(false)
+$('.area-container').replaceWith("<%= j render partial: 'index' %>")
+<% end -%>
