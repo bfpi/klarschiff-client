db707e9b4a993ee74e4359d1b212a688e6197439
Ricardo Oest <oest@bfpi.de>
2017-02-22 08:25:01 +0100

    Ideen- und Probleme-Parameter auch senden, wenn sie nicht gesetzt wurden

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -10,10 +10,10 @@ class ObservationsController < ApplicationController
       begin
         attrs = {}
         attrs.update(area_code: params[:area_code].gsub(' ', ', ')) if params[:area_code]
-        attrs.update(geometry: params[:geometry]) if params[:geometry]
-        attrs.update(problems: params[:problems]) if params[:problems]
+        attrs.update(geometry: params[:geometry])
+        attrs.update(problems: params[:problems].present?)
         attrs.update(problem_service: params[:problem_service].join(', ')) if params[:problem_service]
-        attrs.update(ideas: params[:ideas]) if params[:ideas]
+        attrs.update(ideas: params[:ideas].present?)
         attrs.update(idea_service: params[:idea_service].join(', ')) if params[:idea_service]
         Observation.create(attrs)
       end
