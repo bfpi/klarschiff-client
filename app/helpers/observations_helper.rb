b397f13751fe37fd5ebb3566d98171bbdc443699
Ricardo Oest <oest@bfpi.de>
2017-05-08 12:39:26 +0200

    #1226: Formular für Beobachtungsflächen angepasst

--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -1,5 +1,9 @@
 module ObservationsHelper
-  def service_names(type)
+  def service_names(type) 
     Service.where(extended: true).select { |s| s.group == nil && s.type == type }.map { |s| { id: s.service_code, name: s.service_name } }
+  end
+
+  def service_name(code)
+    Service.where(extended: true).find { |s| s.group == nil && s.service_name == Service.find(code).group }
   end
 end
