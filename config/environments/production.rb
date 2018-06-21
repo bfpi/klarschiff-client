5bb5f73dc3800307442688810098a1f8e87f772a
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2018-06-21 12:21:53 +0200

    Anpassungen für den Betrieb als Prüf- und Protokollclient

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_controller.relative_url_root = '/ppc'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
