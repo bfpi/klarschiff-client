a05c56d4cee29e5f8e86dd499ef4ce48ef86c299
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2018-12-14 15:34:22 +0100

    Protokollfunktion wieder lauffähig gemacht

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -2,6 +2,7 @@ class ProtocolsController < ApplicationController
   def new
     @request = Request.find(params[:request_id])
     @protocol = Protocol.new message_number: @request.id
+    @protocol.user = @user ? @user.name : ''
     @id_list = params[:id_list].try(:map, &:to_i).presence
     respond_to do |format|
       format.html { head :forbidden }
@@ -10,7 +11,7 @@ class ProtocolsController < ApplicationController
   end
 
   def create
-    protocol = Protocol.new((params[:protocol] || {}).update(:user => @user ? @user.name : params[:protocol][:user]))
+    protocol = Protocol.new(params[:protocol])
     ProtocolMailer.protocol(protocol).deliver_now
     @redirect = request_path(params[:request_id], id_list: params[:protocol][:id_list]).html_safe
     @success = I18n.t('protocols.create.success_text')
