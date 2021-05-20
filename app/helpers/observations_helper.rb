b3469615acf458f3989d58d3350faaf14cf3e4e8
rvoss <voss@bfpi.de>
2021-05-20 12:55:51 +0200

    Beobachtungsfläche neues Backoffice

--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -1,13 +1,9 @@
 module ObservationsHelper
   def service_names(type)
-    Service.where(extended: true).select { |s| s.group == nil && s.type == type }.map { |s| { id: s.service_code, name: s.service_name } }
+    Service.where(extended: true).select { |s| s.type == type }.map { |s| { id: s.group_id, name: s.group } }.uniq
   end
 
   def service_name(code)
-    Service.where(extended: true).find { |s| s.group == nil && s.service_name == Service.find(code).group }
-  end
-
-  def service_name_sub(code)
-    Service.where(extended: true).find { |s| s.group == Service.find(code).group && s.service_name == Service.find(code).service_name }
+    Service.find(code)
   end
 end
