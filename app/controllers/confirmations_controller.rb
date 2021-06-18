82fe8a16d8d0f814f1e5d7598a24a0310e172c78
Niels Bennke <bennke@bfpi.de>
2021-06-18 12:05:53 +0200

    Anpassung Confirmations-Logik zur besseren Berücksichtigung von Proxy-Daten

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -23,30 +23,36 @@ class ConfirmationsController < ApplicationController
   private
 
   def confirm(action, confirmation_id)
-    uri = [Settings::ResourceServer.city_sdk[:site], 'requests']
+    uri = ['requests']
     uri << action unless action.blank?
     uri << confirmation_id
     uri << 'confirm.json'
-    json = JSON.parse(JSON.parse(call(uri).read_body.to_json))
+    json = JSON.parse(JSON.parse(call(uri.join('/')).read_body.to_json))
     return render_error(:confirm) if json.blank? || (json.first.is_a?(Hash) && json.first['code'].present?)
     @request = Request.find(json['service_request_id'].to_i)
     render_success(:confirm)
   end
 
   def revoke(confirmation_id)
-    uri = [Settings::ResourceServer.city_sdk[:site], 'requests']
-    uri << confirmation_id
-    uri << 'revoke.json'
+    uri = "requests/#{confirmation_id}/revoke.json"
     json = JSON.parse(JSON.parse(call(uri).read_body.to_json))
     return render_error(:revoke) if json.blank? || (json.first.is_a?(Hash) && json.first['code'].present?)
     render_success(:revoke)
   end
 
-  def call(uri)
-    @uri = URI(uri.join('/'))
-    request = Net::HTTP::Put.new(@uri.path, initheader = {'Content-Type' =>'application/json'})
-    response = Net::HTTP.new(@uri.host, @uri.port).start {|http| http.request(request) }
-    response
+  def call(path)
+    uri = URI.join(Settings::ResourceServer.city_sdk[:site], path)
+    logger.debug uri.inspect
+    request = Net::HTTP::Put.new(uri.path, 'Content-Type' => 'application/json')
+    logger.debug request.inspect
+    logger.debug request['Content-Type'].inspect
+    if (proxy = ENV['HTTP_PROXY'] || ENV['http_proxy']).present? # Workaround for open-uri https-proxy problem
+      proxy = "http://#{proxy}" unless proxy.match(%r{^https?://})
+      p_uri = URI.parse(proxy)
+      Net::HTTP.Proxy p_uri.host, p_uri.port
+    else
+      Net::HTTP
+    end.new(uri.host, uri.port).start { |http| http.request request }
   end
 
   def render_error(template_prefix)
