4cde3024c8044dd4a095292e146f4aae946bdb00
Niels Bennke <bennke@bfpi.de>
2021-06-21 13:27:53 +0000

    Proxy aus Umgebungsdaten fehlertolerant auslesen und anwenden

--- app/jobs/refresh_static_requests_job.rb
+++ app/jobs/refresh_static_requests_job.rb
@@ -2,6 +2,12 @@ class RefreshStaticRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Net::HTTP.get URI.parse("#{Settings::Url.ks_server_url}/requests")
+    if (proxy = ENV['HTTP_PROXY'] || ENV['http_proxy']).present? # Workaround for open-uri https-proxy problem
+      proxy = "http://#{proxy}" unless proxy.match(%r{^https?://})
+      p_uri = URI.parse(proxy)
+      Net::HTTP.Proxy p_uri.host, p_uri.port
+    else
+      Net::HTTP
+    end.get URI.join(Settings::Url.ks_server_url, 'requests')
   end
 end
