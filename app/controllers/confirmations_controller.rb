4903aa6d89eb1eb6f5e8c3bdf08a6e29cf1e1b0f
nbennke <bennke@bfpi.de>
2021-08-23 14:46:13 +0200

    Anpassung Confirmations-Logik (#22)
    * Anpassung und Vereinfachung Logik

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -23,30 +23,27 @@ class ConfirmationsController < ApplicationController
   private
 
   def confirm(action, confirmation_id)
-    uri = [Settings::ResourceServer.city_sdk[:site], 'requests']
+    uri = ['requests']
     uri << action unless action.blank?
     uri << confirmation_id
     uri << 'confirm.json'
-    json = JSON.parse(JSON.parse(call(uri).read_body.to_json))
+    json = JSON.parse(JSON.parse(call(uri.join('/')).read_body.to_json))
     return render_error(:confirm) if json.blank? || (json.first.is_a?(Hash) && json.first['code'].present?)
     @request = Request.find(json['service_request_id'].to_i)
     render_success(:confirm)
   end
 
   def revoke(confirmation_id)
-    uri = [Settings::ResourceServer.city_sdk[:site], 'requests']
-    uri << confirmation_id
-    uri << 'revoke.json'
+    uri = "requests/#{confirmation_id}/revoke.json"
     json = JSON.parse(JSON.parse(call(uri).read_body.to_json))
     return render_error(:revoke) if json.blank? || (json.first.is_a?(Hash) && json.first['code'].present?)
     render_success(:revoke)
   end
 
-  def call(uri)
-    @uri = URI(uri.join('/'))
-    request = Net::HTTP::Put.new(@uri.path, initheader = {'Content-Type' =>'application/json'})
-    response = Net::HTTP.new(@uri.host, @uri.port).start {|http| http.request(request) }
-    response
+  def call(path)
+    uri = URI.join(Settings::ResourceServer.city_sdk[:site], path)
+    request = Net::HTTP::Put.new(uri.path, 'Content-Type' => 'application/json')
+    Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
   end
 
   def render_error(template_prefix)
