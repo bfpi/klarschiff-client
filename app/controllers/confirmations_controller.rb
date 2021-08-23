eb4978980731783e17a936d328b999531572409e
nbennke <bennke@bfpi.de>
2021-08-23 14:44:01 +0200

    Update app/controllers/confirmations_controller.rb

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -42,17 +42,8 @@ class ConfirmationsController < ApplicationController
 
   def call(path)
     uri = URI.join(Settings::ResourceServer.city_sdk[:site], path)
-    logger.debug uri.inspect
     request = Net::HTTP::Put.new(uri.path, 'Content-Type' => 'application/json')
-    logger.debug request.inspect
-    logger.debug request['Content-Type'].inspect
-    if (proxy = ENV['HTTP_PROXY'] || ENV['http_proxy']).present? # Workaround for open-uri https-proxy problem
-      proxy = "http://#{proxy}" unless proxy.match(%r{^https?://})
-      p_uri = URI.parse(proxy)
-      Net::HTTP.Proxy p_uri.host, p_uri.port
-    else
-      Net::HTTP
-    end.new(uri.host, uri.port).start { |http| http.request request }
+    Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
   end
 
   def render_error(template_prefix)
