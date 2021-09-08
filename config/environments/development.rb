181528ee2bc2772630b90bfea746ff754baf4545
rvoss <voss@bfpi.de>
2021-09-08 10:05:08 +0200

    Mailversand in Rails-Env development deaktiviert

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings = {
+    location: Rails.root.join('tmp/mail')
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
