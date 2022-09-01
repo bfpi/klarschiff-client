e97d22c63274711d11a1b45e5cd3d33cde6e8cd2
ro3t <voss@bfpi.de>
2022-09-01 08:50:46 +0200

    Trac #31: Fokussierung der Klarschiff-Karte (#56)
    Co-authored-by: Niels Bennke <bennke@bfpi.de>

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -5,6 +5,7 @@ class AreasController < ApplicationController
       format.js do
         @context = params[:context]
         area_code = params[:area_code]
+        region = params[:region]
         geom = params[:geometry]
         if params[:from_form].present?
           if (@context == 'districts' && area_code.blank?) || (@context == 'draw' && geom.blank?)
@@ -18,6 +19,10 @@ class AreasController < ApplicationController
         conds = {}
         conds.update area_code: Array.wrap(params[:area_code]).join(', ')
         conds.update with_districts: params[:with_districts] unless params[:with_districts].blank?
+        if params[:region].present?
+          conds.update search_class: 'authority'
+          conds.update regional_key: params[:region]
+        end
         conds.update center: params[:center] unless params[:center].blank?
         conds.update limit: Settings::Client.observations_area_limit
         areas = Area.where(conds)
