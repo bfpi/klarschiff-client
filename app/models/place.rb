c41a5b345b713b5d7c2d7b42d79f7733753401eb
rvoss <voss@bfpi.de>
2023-10-09 10:53:29 +0200

    Gemeindebezeichnung in der Suchfunktion verbessert

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,24 @@ class Place
   attr_accessor :type, :properties, :geometry
 
   def as_json(options = {})
-    { label: properties['objektgruppe'] == 'Gemeindeteil' ? properties['_title_'].split(', ')[-1] : properties['abkuerzung'].blank? ? properties['_title_'].split(', ')[-1] : properties['_title_'].split(', ')[-1] + ' (' + properties['abkuerzung'] + ')', bbox: geometry['type'] == 'Point' ? [geometry['coordinates'][0],geometry['coordinates'][1],geometry['coordinates'][0],geometry['coordinates'][1]] : [geometry['coordinates'][0][0][0],geometry['coordinates'][0][0][1],geometry['coordinates'][0][2][0],geometry['coordinates'][0][2][1]], transform_bbox: geometry['transform_bbox'], feature_id: properties['feature_id'] }
+    { label: label(properties), bbox: bbox(geometry), transform_bbox: geometry['transform_bbox'], feature_id: properties['feature_id'] }
+  end
+
+  private
+  def label(properties)
+    title = properties['_title_']
+    title_without_community = title.split(', ')[-1]
+    case properties['objektgruppe']
+    when 'Gemeinde'
+      title
+    when 'Gemeindeteil'
+      title_without_community
+    else
+      properties['abkuerzung'].blank? ? title_without_community : title_without_community + ' (' + properties['abkuerzung'] + ')'
+    end
+  end
+
+  def bbox(geometry)
+    geometry['type'] == 'Point' ? [geometry['coordinates'][0],geometry['coordinates'][1],geometry['coordinates'][0],geometry['coordinates'][1]] : [geometry['coordinates'][0][0][0],geometry['coordinates'][0][0][1],geometry['coordinates'][0][2][0],geometry['coordinates'][0][2][1]]
   end
 end
