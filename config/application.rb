3471e654259773f14fd49ea482dfefc964de5eff
rvoss <voss@bfpi.de>
2025-11-25 09:20:42 +0100

    WIP: Controller Exteions

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module KlarschiffClient
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    config.autoload_paths << "#{root}/overlay/config"
+    #config.autoload_paths << "#{root}/overlay/controllers"
+    p config.autoload_paths.inspect
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
