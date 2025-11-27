d4d926e93e611c17d6e1b5f28b05d578561dbbe0
rvoss <voss@bfpi.de>
2025-11-27 15:14:20 +0100

    Statistik-Badges

--- app/controllers/starts_controller.rb
+++ app/controllers/starts_controller.rb
@@ -1,15 +1,11 @@
 class StartsController < ApplicationController
+  include ApplicationHelper
+
   def show
     @request_id = params[:request]
     return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path(request: @request_id, mobile: true) }" if login_required?
     return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path }" if Settings::Client.skip_extra_homepage
     recent_requests
-    @current_count = Request.count(also_archived: false, detailed_status: states, keyword: 'problem, idea')
-    @overall_count = Request.count(also_archived: true, detailed_status: states, keyword: 'problem, idea')
-    @newest_count = Request.count(also_archived: true, detailed_status: states, keyword: 'problem, idea',
-                                  start_date: Date.today - 30)
-    @processed_count = Request.count(also_archived: true, detailed_status: 'PROCESSED', keyword: 'problem, idea',
-                                     start_date: Date.today - 30)
   end
 
   def recent_requests
@@ -18,8 +14,4 @@ class StartsController < ApplicationController
     @recent_requests_col = "col-#{ 12/Settings::Client.recent_requests_per_row.to_i }"
   end
 
-  private
-  def states
-   Settings::Map.default_requests_states.strip.split(', ').select { |s| s != 'PENDING' }.join(', ')
-  end
 end
