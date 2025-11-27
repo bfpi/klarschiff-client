b3af656ac98262748a76c34ae38fcd4f0176355d
rvoss <voss@bfpi.de>
2025-11-27 15:20:51 +0100

    Mobile-Start

--- app/controllers/starts_controller.rb
+++ app/controllers/starts_controller.rb
@@ -4,7 +4,7 @@ class StartsController < ApplicationController
   def show
     @request_id = params[:request]
     return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path(request: @request_id, mobile: true) }" if login_required?
-    return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path }" if Settings::Client.skip_extra_homepage
+    return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path }" if Settings::Client.skip_extra_homepage && !@mobile
     recent_requests
   end
 
