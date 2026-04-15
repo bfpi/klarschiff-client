71b90ad8a9cbe7a4a21099919261aefff500e0b7
Niels Bennke <bennke@bfpi.de>
2026-04-15 11:41:19 +0000

    Fix CodeStyle

--- app/controllers/starts_controller.rb
+++ app/controllers/starts_controller.rb
@@ -3,15 +3,16 @@ class StartsController < ApplicationController
 
   def show
     @request_id = params[:request]
-    return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path(request: @request_id, mobile: true) }" if login_required?
-    return redirect_to "#{ Settings::Url.ks_server_url }#{ map_path }" if Settings::Client.skip_extra_homepage && !@mobile
+    if login_required?
+      return redirect_to "#{Settings::Url.ks_server_url}#{map_path request: @request_id, mobile: true}"
+    end
+    return redirect_to "#{Settings::Url.ks_server_url}#{map_path}" if Settings::Client.skip_extra_homepage && !@mobile
     recent_requests
   end
 
   def recent_requests
-    @recent_requests = Request.where(max_requests: Settings::Client.recent_requests, detailed_status: states, keyword: 'problem, idea',
-                              with_picture: true)
-    @recent_requests_col = "col-#{ 12/Settings::Client.recent_requests_per_row.to_i }"
+    @recent_requests = Request.where(max_requests: Settings::Client.recent_requests, detailed_status: states,
+                                     keyword: 'problem, idea', with_picture: true)
+    @recent_requests_col = "col-#{12 / Settings::Client.recent_requests_per_row.to_i}"
   end
-
 end
